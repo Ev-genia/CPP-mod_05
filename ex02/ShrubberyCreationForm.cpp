6cca3a654a0b706e8947080eda5915956f742cd0
Евгения Костарева <eugenia@mac-book-air-2020.local>
2022-10-26 00:25:17 +0300

    add comment to ShrubberyCreationForm::execute

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -6,7 +6,7 @@
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 13:24:21 by mlarra            #+#    #+#             */
-/*   Updated: 2022/10/26 00:14:48 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/10/26 00:24:08 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,18 @@ ShrubberyCreationForm::~ShrubberyCreationForm()
 {
 }
 
-ShrubberyCreationForm::ShrubberyCreationForm(std::string enterTarget): Form("NoNameShrubbery", 145, 137)
+ShrubberyCreationForm::ShrubberyCreationForm(std::string enterTarget): 
+												Form("NoNameShrubbery", 145, 137)
 {
 	target = enterTarget;
 }
 
 void	ShrubberyCreationForm::execute(Bureaucrat const & executer) const
 {
+// Вы должны убедиться, что форма подписана и что уровень бюрократа, 
+// пытающегося заполнить форму, достаточно высок. 
+// В противном случае создайте соответствующее исключение.
+
 	std::ofstream	outfile;
 
 	outfile.open(target + "_shrubbery");
